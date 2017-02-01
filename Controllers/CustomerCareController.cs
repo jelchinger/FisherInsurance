38bae1c02802e99a09eea3c94efd044a38d2c1cc
Jared Elchinger <Jared Elchinger>
2017-02-01 17:59:27 -0500

    Added ClaimHistory, NewClaim to CustomerCareController

--- Controllers/CustomerCareController.cs
+++ Controllers/CustomerCareController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 
+[Route("customer")]
 public class CustomerCareController : Controller
 
 {
@@ -8,9 +9,21 @@ public class CustomerCareController : Controller
         return Ok("This is the index of the CustomerCareController");
     }
 
-    [Route("CustomerCare/Claims")]
+    [Route("Claims")]
     public IActionResult Quotes() 
     {
         return Ok("Claims");
+    }
+
+    [Route("Claims/fileclaim")]
+    public IActionResult NewClaim() 
+    {
+        return Ok("NewClaim");
+    }
+
+    [Route("Claims/myclaims")]
+    public IActionResult ClaimHistory() 
+    {
+        return Ok("ClaimHistory");
     }
 }
