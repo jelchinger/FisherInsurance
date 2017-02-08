56c03b41ba85249dbc410763cc50bd89b9478820
Jared Elchinger <Jared Elchinger>
2017-02-08 18:06:12 -0500

    models

---
+++ Models/Quote.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace FisherInsurance.Models
+{
+    public class Quotes 
+    {
+        public int Id { get; set; }
+
+        public string Product { get; set;}
+
+        public DateTime ExpireDate { get; set;}
+        
+        public decimal Price { get; set; }
+        
+    }
+}
